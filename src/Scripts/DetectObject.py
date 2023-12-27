0d1474fc0a9c9e38159b405caac8be90f6d8e776
Félix Chenette-Stewart <felix@nexumnet.com>
2023-12-26 20:55:56 -0500

    OOP base

---
+++ src/Scripts/DetectObject.py
@@ -0,0 +1,5 @@
+import cv2
+
+class DetectObject:
+    def __init__(self, data):
+        self.data = data
---
+++ src/Scripts/DetectOrientation.py
@@ -0,0 +1,5 @@
+import cv2
+
+class DetectOrientation:
+    def __init__(self, data):
+        self.data = data
